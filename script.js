df514c64c9f7959044996d8ef169315b4f15f5ac
tihi <77891364+JustTizian@users.noreply.github.com>
2025-07-22 12:39:07 +0200

    Implement card pairs and shuffling

--- script.js
+++ script.js
@@ -4,45 +4,27 @@ function $(query) {
 
 const cardContainer = $('#card-container')
 
-const cardsData = [
-    {
-        symbol: "1",
-        turned: false
-    },
-    {
-        symbol: "2",
-        turned: false
-    },
-    {
-        symbol: "3",
-        turned: false
-    },
-    {
-        symbol: "4",
-        turned: false
-    },
-    {
-        symbol: "5",
-        turned: false
-    },
-    {
-        symbol: "6",
-        turned: false
-    },
-    {
-        symbol: "7",
-        turned: false
-    },
-    {
-        symbol: "8",
-        turned: false
-    }
-]
+const cardSymbols = ["👾","💩","😶‍🌫️","🫡","🎶","😎","❤️","🥶"]
 
-const cards = cardsData.map(card => {
+let cards;
+const cards1 = cardSymbols.map(_symbol => {
+    const symbol = _symbol
+    const turned = false
     const button = document.createElement("button")
-    return { ...card, button: button }
+    return { symbol, turned, button}
 })
+
+const cards2 = cardSymbols.map(_symbol => {
+    const symbol = _symbol
+    const turned = false
+    const button = document.createElement("button")
+    return { symbol, turned, button}
+})
+
+cards = [...cards1, ...cards2]
+cards.sort(() => Math.ceil(Math.random() * 10) - 5)
+
+const flippedCards = []
 
 cards.forEach(card => card.button.addEventListener("click", () => flipCard(card)))
 
@@ -56,6 +38,12 @@ function updateUI() {
 
 function flipCard(card) {
     card.turned = !card.turned
+    flippedCards.push(card)
+    if(flippedCards.length === 2){
+        console.log(flippedCards[0].symbol === flippedCards[1].symbol)
+        flippedCards.pop()
+        flippedCards.pop()
+    }
     updateUI()
 }
 
